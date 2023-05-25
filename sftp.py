8a449664981ee1d2595d9f60987edb16e0507df7
Misha <mishagrigoryan@gmail.com>
2023-05-25 18:49:22 -0400

    added sftp s3 bucket

---
+++ sftp.py
@@ -0,0 +1,42 @@
+import psycopg2
+import csv
+import boto3
+from datetime import date
+
+conn = psycopg2.connect(
+    host="database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com",
+    database="postgres",
+    user="postgres",
+    password="Manonthemoon123"
+)
+
+cur = conn.cursor()
+
+query = "SELECT category AS Category, AVG(price) AS Price FROM public.gundeals GROUP BY category"
+cur.execute(query)
+
+rows = cur.fetchall()
+
+current_date = date.today().strftime("%Y-%m-%d")
+
+csv_filename = f"gundeals_average_prices_{current_date}.csv"
+
+with open(csv_filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([desc[0] for desc in cur.description]) 
+    writer.writerows(rows)  
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id='AKIAULBW5F545X3NIB54',
+    aws_secret_access_key='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',
+    region_name='us-east-2' 
+
+bucket_name = "gundealsbucket1"
+
+s3.upload_file(csv_filename, bucket_name, csv_filename)
+
+cur.close()
+conn.close()
+
+print(f"CSV file '{csv_filename}' uploaded to S3 bucket '{bucket_name}' successfully.")
