9527a1181fdc6767195e55c64f13356509222519
Misha <mishagrigoryan@gmail.com>
2023-05-27 22:48:00 -0400

    security

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# Reddit API
+reddit_username = "Searchbot_01"
+reddit_password = "aaaa1111"
+reddit_client_id = "Ai32qfXNqvGuMEvHFFMlAw"
+reddit_client_secret = "IG5XKjyUGkcG2cgXfBSwVvalMTxFRg"
+
+# PostgreSQL
+pg_host = "database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com"
+pg_database = "postgres"
+pg_user = "postgres"
+pg_password = "Manonthemoon123"
+
+# Twilio
+twilio_account_sid = "ACd2079bd46f13974225b28ef06255af01"
+twilio_auth_token = "fc48aa972b023395d213c11a148238c1"
+twilio_phone_number = "+18334633894"
+twilio_to_number = "14232272113"
